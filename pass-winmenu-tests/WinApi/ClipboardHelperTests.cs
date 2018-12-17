71be3d0232247cd13f2dfdabd83cbdda02121248
Johan <johan@geluk.io>
2018-12-17 03:08:22 +0100

    Clipboard tests: Comment out temporarily disabled code
    Comment out temporarily disabled code to stop the compiler from generating
    warnings.

--- pass-winmenu-tests/WinApi/ClipboardHelperTests.cs
+++ pass-winmenu-tests/WinApi/ClipboardHelperTests.cs
@@ -16,16 +16,15 @@ namespace PassWinmenu.WinApi
 		{
 			// TODO: Find a way to test this.
 			// The code below doesn't work because we have no Application reference.
-			return;
 
-			var clipboard = new ClipboardHelper();
-			var content = Guid.NewGuid().ToString();
+			//var clipboard = new ClipboardHelper();
+			//var content = Guid.NewGuid().ToString();
 
-			Assert.AreNotEqual(clipboard.GetText(), content);
+			//Assert.AreNotEqual(clipboard.GetText(), content);
 
-			clipboard.Place(content, TimeSpan.FromSeconds(1));
+			//clipboard.Place(content, TimeSpan.FromSeconds(1));
 
-			Assert.AreEqual(clipboard.GetText(), content);
+			//Assert.AreEqual(clipboard.GetText(), content);
 		}
 
 		[TestMethod, TestCategory(Category)]
@@ -33,27 +32,26 @@ namespace PassWinmenu.WinApi
 		{
 			// TODO: Find a way to test this.
 			// The code below doesn't work because we have no Application reference.
-			return;
 
-			var clipboard = new ClipboardHelper();
-			var content = Guid.NewGuid().ToString();
+			//var clipboard = new ClipboardHelper();
+			//var content = Guid.NewGuid().ToString();
 
-			var beforeText = clipboard.GetText();
+			//var beforeText = clipboard.GetText();
 
-			clipboard.Place(content, TimeSpan.FromSeconds(1));
-			Assert.AreNotEqual(beforeText, clipboard.GetText());
-			Thread.Sleep(1100);
+			//clipboard.Place(content, TimeSpan.FromSeconds(1));
+			//Assert.AreNotEqual(beforeText, clipboard.GetText());
+			//Thread.Sleep(1100);
 
-			var waitAttempts = 20;
-			for (var i = 0; i < waitAttempts && clipboard.GetText() == content; i++)
-			{
-				Thread.Sleep(100);
-			}
-			var clipText = clipboard.GetText();
+			//var waitAttempts = 20;
+			//for (var i = 0; i < waitAttempts && clipboard.GetText() == content; i++)
+			//{
+			//	Thread.Sleep(100);
+			//}
+			//var clipText = clipboard.GetText();
 
-			Assert.AreNotEqual(clipText, content, "Clipboard was not cleared.");
+			//Assert.AreNotEqual(clipText, content, "Clipboard was not cleared.");
 
-			Assert.AreEqual(beforeText, clipText);
+			//Assert.AreEqual(beforeText, clipText);
 		}
 	}
 }
