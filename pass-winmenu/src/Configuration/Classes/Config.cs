b401b84d0d1076091c421e11213579e4add80132
Johan <johan@geluk.io>
2018-12-17 03:10:23 +0100

    Config version: Add comment explaining versioning scheme

--- pass-winmenu/src/Configuration/Classes/Config.cs
+++ pass-winmenu/src/Configuration/Classes/Config.cs
@@ -40,6 +40,14 @@ namespace PassWinmenu.Configuration
 		public InterfaceConfig Interface { get; set; } = new InterfaceConfig();
 
 		public bool CreateLogFile { get; set; } = false;
+		/// <summary>
+		/// Determines the current version of the configuration file.
+		/// Config file versions run synchronously with pass-winmenu versions,
+		/// but not every pass-winmenu update will also bump the configuration file version.
+		/// This only happens when there are changes preventing users from re-using an older
+		/// configuration file for a newer version of pass-winmenu. In that case, the new
+		/// configuration file version will be set to the latest version of pass-winmenu.
+		/// </summary>
 		public string ConfigVersion { get; set; }
 	}
 }
