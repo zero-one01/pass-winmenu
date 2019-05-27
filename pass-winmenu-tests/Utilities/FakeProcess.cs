d112f08f9372f1d5413d9a4ff230c77c99b9275a
Johan <johan@geluk.io>
2019-05-27 02:29:23 +0200

    Add GpgTransportTests

--- pass-winmenu-tests/Utilities/FakeProcess.cs
+++ pass-winmenu-tests/Utilities/FakeProcess.cs
@@ -16,13 +16,13 @@ namespace PassWinmenu.Utilities
 			return ExitTime < timeout;
 		}
 
-		public StreamWriter StandardInput { get; } = new StreamWriter(Stream.Null);
-		public StreamReader StandardOutput { get; } = new StreamReader(Stream.Null);
+		public StreamWriter StandardInput { get; set; } = new StreamWriter(Stream.Null);
+		public StreamReader StandardOutput { get; set; } = new StreamReader(Stream.Null);
 		public StreamReader StandardError { get; set; } = new StreamReader(Stream.Null);
 		public string MainModuleName { get; } = string.Empty;
 		public int Id { get; } = 1;
 		public DateTime StartTime { get; } = DateTime.Now;
-		public int ExitCode { get; } = 0;
+		public int ExitCode { get; set; } = 0;
 
 		public TimeSpan ExitTime { get; set; }
 	}
--- pass-winmenu-tests/Utilities/FakeProcessBuilder.cs
+++ pass-winmenu-tests/Utilities/FakeProcessBuilder.cs
@@ -9,13 +9,19 @@ namespace PassWinmenu.Utilities
 	{
 		private TimeSpan exitTime;
 		private StreamReader standardError;
+		private StreamReader standardOutput;
+		private int exitCode;
+		private Stream inputStream;
 
 		public FakeProcess Build()
 		{
 			return new FakeProcess
 			{
+				ExitCode = exitCode,
 				ExitTime = exitTime,
-				StandardError = standardError
+				StandardError = standardError,
+				StandardOutput = standardOutput,
+				StandardInput = inputStream == null ? null : new StreamWriter(inputStream)
 			};
 		}
 
@@ -28,6 +34,24 @@ namespace PassWinmenu.Utilities
 		public FakeProcessBuilder WithExitTime(TimeSpan exitTime)
 		{
 			this.exitTime = exitTime;
+			return this;
+		}
+
+		public FakeProcessBuilder WithStandardOutput(string standardOutput)
+		{
+			this.standardOutput = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(standardOutput)));
+			return this;
+		}
+
+		public FakeProcessBuilder WithExitCode(int exitCode)
+		{
+			this.exitCode = exitCode;
+			return this;
+		}
+
+		public FakeProcessBuilder WithStandardInput(Stream inputStream)
+		{
+			this.inputStream = inputStream;
 			return this;
 		}
 	}
