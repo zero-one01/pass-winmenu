241490952a96b231047feed33928b3295a4b6fdf
Johan <johan@geluk.io>
2020-11-09 20:27:54 +0100

    Remove unused [Serializable] attribute

--- pass-winmenu/src/Configuration/RuntimeConfigurationError.cs
+++ pass-winmenu/src/Configuration/RuntimeConfigurationError.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace PassWinmenu.Configuration
 {
-	[Serializable]
 	public class RuntimeConfigurationError : Exception
 	{
 		public RuntimeConfigurationError(string message) : base(message)
